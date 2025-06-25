0e2789b0ea7bd73d3621bf0af1bf9ea06521935c
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-06-25 13:28:16 +0530

    migrations

--- core/home/models.py
+++ core/home/models.py
@@ -6,5 +6,4 @@ class student(models.Model):
     age=models.IntegerField()
     email=models.EmailField()
     address=models.TextField(null=True, blank=True)
-    image=models.ImageField()
     file=models.FileField()
